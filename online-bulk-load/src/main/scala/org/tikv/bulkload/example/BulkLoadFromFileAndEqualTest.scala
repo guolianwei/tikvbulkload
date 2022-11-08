3b6db6d9fb6d045a92d8d15265f52bff0f9dde6e
guolw <2926729767@qq.com>
2022-11-08 15:14:44 +0800

    增加前缀写入批量数据，并进行批量查询比对测试！

--- online-bulk-load/src/main/scala/org/tikv/bulkload/example/BulkLoadFromFileAndEqualTest.scala
+++ online-bulk-load/src/main/scala/org/tikv/bulkload/example/BulkLoadFromFileAndEqualTest.scala
@@ -30,6 +30,7 @@ import java.io.{FileInputStream, ObjectInputStream}
 import java.nio.charset.Charset
 import java.util
 import java.util.Optional;
+import scala.collection.mutable.ListBuffer
 
 object BulkLoadFromFileAndEqualTest {
 
@@ -46,6 +47,7 @@ object BulkLoadFromFileAndEqualTest {
     prefix = args(1)
     var batchQ = args(2).toInt
     var dataPath = args(3)
+    var onlyGet = args(4).toInt
     println(s"""
          |*****************
          |pdaddr=$pdaddr
@@ -55,7 +57,8 @@ object BulkLoadFromFileAndEqualTest {
          |exit=$exit
          |exact=$exact
          |months=$months
-         |months=$batchQ
+         |batchQ=$batchQ
+         |onlyGet=$onlyGet
          |*****************
          |""".stripMargin)
 
@@ -71,27 +74,25 @@ object BulkLoadFromFileAndEqualTest {
       .setIfMissing("spark.driver.maxResultSize", "2048m")
 
     val loader = new RawKVBulkLoader(pdaddr)
+    if (onlyGet != 1) {
+      val spark = SparkSession.builder.config(sparkConf).getOrCreate()
+      val rdd = produceRdd(months, dataPath, prefix, spark);
+      loader.bulkLoad(rdd)
+    }
 
-    val spark = SparkSession.builder.config(sparkConf).getOrCreate()
-    val rdd = produceRdd(months,dataPath, prefix, spark);
-    loader.bulkLoad(rdd)
     val end = System.currentTimeMillis()
     println(s"total time: ${(end - start) / 1000}s")
     println(s"begin to check value")
-
+    val checkBegin = System.currentTimeMillis();
     val conf = TiConfiguration.createRawDefault("10.17.39.100:2379")
     conf.setRawKVBatchWriteTimeoutInMS(10000)
     conf.setDBPrefix("nucleic_information")
     val session = TiSession.create(conf)
     val client = session.createRawClient
-    var sum: Int = 0
-    var seqSum = Seq.empty[(String, Int)];
-    for (i <- 1 to 1200) {
-      val tuple = checkKey(prefix,dataPath,batchQ, i, client)
-      val i1 = tuple._1
-      sum += i1
-      if (sum % 100000 == 0) println(s"sum is $sum")
-      seqSum :+ tuple._2
+    var seqSum = new ListBuffer[(String, Int)];
+    for (i <- 1 to months) {
+      val tuple = checkKey(prefix, dataPath, batchQ, i, client)
+      seqSum ++= tuple
     }
     seqSum
       .groupBy(_._1)
@@ -103,24 +104,31 @@ object BulkLoadFromFileAndEqualTest {
       })
       .foreach(f => println(f._1 + " -> " + f._2))
     println(s"check value end")
-
+    val checkEnd = System.currentTimeMillis()
+    println(s"check value end ${(checkEnd - checkBegin) / 1000} s")
     while (!exit) {
       Thread.sleep(1000)
     }
   }
 
-  private def checkKey(prefix:String,dataPath:String,batchQ: Int, i: Int, client: RawKVClient): (Int, Seq[(String, Int)]) = {
-    var equalsCount: Int = 0
+  private def checkKey(
+      prefix: String,
+      dataPath: String,
+      batchQ: Int,
+      i: Int,
+      client: RawKVClient): List[(String, Int)] = {
     val oFileName = dataPath + i;
     println("checking " + oFileName)
     val in = new ObjectInputStream(new FileInputStream(oFileName))
     val saveNick = in.readObject().asInstanceOf[Array[(Array[Byte], Array[Byte])]]
-    var seq = Seq.empty[(String, Int)]
-    val stringToInt = saveNick.zipWithIndex
+
+    var resList = new ListBuffer[(String, Int)]
+    val stringToInt1 = saveNick.zipWithIndex
       .map(f => {
         (f._2 % batchQ, f._1)
       })
       .groupBy(_._1)
+      .toList
       .flatMap(x => {
         val kvs = x._2
         val batchList: util.List[ByteString] =
@@ -128,31 +136,46 @@ object BulkLoadFromFileAndEqualTest {
         val valuesKeV = kvs
           .map(f => {
             val strKey = new String(f._2._1)
-            batchList.add(ByteString.copyFromUtf8(prefix+strKey))
-            (strKey, new String(f._2._2))
+            val keyFinal = prefix + strKey;
+            batchList.add(ByteString.copyFromUtf8(keyFinal))
+            (keyFinal, new String(f._2._2))
           })
           .toMap
         val pairs = client.batchGet(batchList)
-        val seq: Seq[(String, Int)] = getTestedValues(equalsCount, pairs, valuesKeV)
-        seq
+        val resSize = pairs.size()
+        if (resSize > 0) {
+          println(s"get [$resSize] values from tikv by keys")
+          resList = getTestedValues(pairs, valuesKeV)
+          val size = resList.size
+          println(s"get [$size] cmp res from tikv by keys")
+        } else {
+          println("can't get value from tikv by key" + pairs)
+        }
+        val stringToInt = resList
+          .groupBy(_._1)
+          .map(x => {
+            val tuple = x._2.reduce((y1, y2) => {
+              (x._1, y1._2 + y2._2)
+            })
+            (x._1, tuple._2)
+          })
+        stringToInt.toList
       })
+    val stringToInt = stringToInt1
       .groupBy(_._1)
       .map(x => {
         val i = x._2.reduce((y1, y2) => (x._1, y1._2 + y2._2))
         (x._1, i._2)
       })
-
-    for (elem <- stringToInt) {
-      seq :+ elem
-    }
-    (equalsCount, seq)
+      .toList
+    stringToInt
   }
 
   private def getTestedValues(
-      equalsCount: Int,
       pairs: util.List[Kvrpcpb.KvPair],
-      valuesKeV: Map[String, String]) = {
-    val seq = Seq.empty[(String, Int)]
+      valuesKeV: Map[String, String]): ListBuffer[(String, Int)] = {
+
+    val listBuffer = new ListBuffer[(String, Int)]
     pairs.forEach(pair => {
       val key = pair.getKey
       val value = pair.getValue
@@ -160,9 +183,6 @@ object BulkLoadFromFileAndEqualTest {
         val valueInTiKvs = value.toStringUtf8
         val valueInFile = valuesKeV.get(key.toStringUtf8)
         if (valueInTiKvs.equals(valueInFile.get)) {
-          if (equalsCount % 10000 == 0) {
-            println(equalsCount)
-          }
           ("eq", 1)
         } else {
           println(
@@ -173,13 +193,13 @@ object BulkLoadFromFileAndEqualTest {
         println(s"not found value in tikv by key $key")
         ("nf", 1)
       }
-      seq :+ res
+      listBuffer += res
     })
-    seq
+    listBuffer
   }
 
-  private def produceRdd(months: Int,dataPath:String, prefix: String, spark: SparkSession) = {
-    new AarrayFileJdbcRDD[(Array[Byte], Array[Byte])](spark.sparkContext, months, prefix,dataPath);
+  private def produceRdd(months: Int, dataPath: String, prefix: String, spark: SparkSession) = {
+    new AarrayFileJdbcRDD[(Array[Byte], Array[Byte])](spark.sparkContext, months, prefix, dataPath);
   }
 
   private def genKey(i: Long): String = f"$i%016d"
